6bffa77c6f1023e90e903081ca6f968028138904
roflowhale <contactbrianzhang@berkeley.edu>
2016-04-26 23:52:40 -0700

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,60 @@ $(document).ready(function(){
         if (winWidth < 1050){
             $('.logo').addClass('logo-center');
             $('.text-container').css('width','75%');
-            $('.text-container').css('top','30%');
+            $('.text-container').css('top','45%');
             $('.hero').css('width','75%');
         }else if($(".logo").hasClass('logo-center')){
             $(".logo").removeClass('logo-center');   
-            $('.text-container').css('width','960px');
-            $('.hero').css('width','960px');
+            $('.text-container').css('width','700px');
+            $('.hero').css('width','700px');
             $('.text-container').css('top','50%');
         }else{
             return false;
         }   
     });
- 
+    
+    $("#left").click(function(){
+        if(!$("#p1.partner").hasClass('hide')){
+            return false;
+        }else if(!$("#p2.partner").hasClass('hide')){
+            $("#p2.partner").addClass('hide');
+            $("#p1.partner").removeClass('hide');
+        }
+        else if(!$("#p3.partner").hasClass('hide')){
+            $("#p3.partner").addClass('hide');
+            $("#p2.partner").removeClass('hide');
+        }else if(!$("#p4.partner").hasClass('hide')){
+            $("#p4.partner").addClass('hide');
+            $("#p3.partner").removeClass('hide');
+        }else if(!$("#p5.partner").hasClass('hide')){
+            $("#p5.partner").addClass('hide');
+            $("#p4.partner").removeClass('hide');
+        }
+    });
+    
+    $("#right").click(function(){
+        if(!$("#p5.partner").hasClass('hide')){
+            return false;
+        }else if(!$("#p4.partner").hasClass('hide')){
+            $("#p4.partner").addClass('hide');
+            $("#p5.partner").removeClass('hide');
+        }
+        else if(!$("#p3.partner").hasClass('hide')){
+            $("#p3.partner").addClass('hide');
+            $("#p4.partner").removeClass('hide');
+        }
+        else if(!$("#p2.partner").hasClass('hide')){
+            $("#p2.partner").addClass('hide');
+            $("#p3.partner").removeClass('hide');
+        }
+        else if(!$("#p1.partner").hasClass('hide')){
+            $("#p1.partner").addClass('hide');
+            $("#p2.partner").removeClass('hide');
+        }
+        else {
+            return false;
+        }
+    });
+    
     
 });
